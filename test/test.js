7536cacd8c806a1b9cda1fbc003fb028bc43acb1
Rajkumar <b-rajkumar@users.noreply.github.com>
2023-05-17 12:46:10 +0530

    TODO.txt is added in the template

--- test/test.js
+++ test/test.js
@@ -1,10 +1,9 @@
 const {deepStrictEqual} = require('assert');
 const {describe, it} = require('node:test');
-const {one} = require('../src/source.js');
 
 describe('sample test', function() {
   it('should return true', function() {
-    deepStrictEqual(one, 1);
+    deepStrictEqual(1, 1);
   });
 });
 
